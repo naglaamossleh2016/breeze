07171becf0ced9a541a6f261c4aa5c52be92b99f
naglaamossleh2016 <naglaa.mossleh@gmail.com>
2024-05-05 20:22:41 +0300

    auth using breeze

---
+++ database/migrations/2024_05_04_232417_clone_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Db;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+      DB::statement('CREATE TABLE svu_user LIKE users;'); // Create the new table with the same structure
+      DB::statement('INSERT INTO svu_user SELECT * FROM users;'); // Copy data from the original table
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::dropIfExists('svu_user');
+    }
+};
